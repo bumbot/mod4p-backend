4d95bfc29242b5cfd429034d088309bea760c7a3
willmharris <willmharris0@gmail.com>
2020-04-13 12:38:48 -0400

    basic genius response working

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 class MusicsController < ApplicationController
     def index
         musics = Music.all
@@ -8,7 +10,21 @@ class MusicsController < ApplicationController
     def show
         music = Music.find(params[:id])
 
-
         render json: music if music
     end
+
+    def get_song 
+        token = "-YbFQTXMw5wHgbhmz9m5_F0FqM7S8zbpW43DldPqw9JB0LzU0zplcikqnc--a6e4"
+        response = RestClient.get('https://api.genius.com/search?q=Hello%20Adele', {
+            "User-Agent": "CompuServe Classic/1.22",
+            "Accept": "application/json",
+            "Host": "api.genius.com",
+            "Authorization": "Bearer #{token}"
+        })
+        
+        json_body = JSON.parse(response.body)
+        puts json_body 
+        render json: json_body
+
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :musics
   resources :playlists
   resources :users
+  get '/get_song', action: "get_song", controller: 'musics'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
